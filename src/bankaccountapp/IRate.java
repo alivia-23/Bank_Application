f060efd127b762d3e0f60d4ac23d883d6b1a7056
alivia-23 <59635643+alivia-23@users.noreply.github.com>
2020-05-29 17:42:22 -0700

    Update IRate.java

--- src/bankaccountapp/IRate.java
+++ src/bankaccountapp/IRate.java
@@ -2,7 +2,7 @@ package bankaccountapp;
 
 public interface IRate {
 	
-	// write a method that returns interest rate
+	// method returns interest rate
 	default double getBaseRate() {
 		return 2.5;
 	}
